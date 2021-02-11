bed8dc72391d7c831fbca31b7554a9c47b086251
rafliramadhan1 <rafli.ramadhan.me@gmail.com>
2021-02-12 06:08:44 +0700

    oo

--- financialstatement/settings.py
+++ financialstatement/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'qi5i24e4h52*c*!^zatn$brlvr4$9#3q+d*j6xbrn*7n#hj5vg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["glacial-coast-08306.herokuapp.com", "127.0.0.1"]
 
